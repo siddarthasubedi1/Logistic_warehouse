53e2554b45f0ae8877b3a87400ddd72cc820ce9d
siddartha <siddarthasubedi1@gmail.com>
2026-08-20 10:19:41 +0545

    initial commit by siddartha

---
+++ frontend/src/components/practice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const practice = () => {
+    return (
+        <div>
+            <h1>Hi this is a practice component</h1>
+        </div>
+    )
+}
+
+export default practice
